7e38610c872cd3d223f27dba32550dca269038e0
Sarthak044 <46262012+Sarthak044@users.noreply.github.com>
2021-04-21 13:29:35 +0000

    Add files via upload
    A Minor Project Made by me and my friends 
    -Sarthak 
    -Tanveer
    -Anshuman

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+pip install argparse
+pip install re
+pip install socket
+pip install ipaddress
+pip install urllib
+pip install requests 
+pip install scapy 
+ 
