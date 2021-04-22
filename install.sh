7a71dba5b54c2d5f4dea5e374c62a19aa3fa9a0b
iwtdeiml <43274777+iwtdeiml@users.noreply.github.com>
2021-04-22 10:25:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update
+sudo apt-get update -y && sudo apt-get install python3 && sudo apt-get install python3-pip
 pip install argparse
 pip install re
 pip install socket
